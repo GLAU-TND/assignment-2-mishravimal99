e41f5b476bc3b1afa6290a9aa48a5f543be8b1b6
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-03-28 12:57:27 +0530

    Main method added

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -14,12 +14,12 @@ import problem3.student.Student;
 public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue queue = new MyPriorityQueue();
-        queue.enqueue(new Student("Tushar",58));
-        queue.enqueue(new Student("Tanishq",23));
-        queue.enqueue(new Student("Pavit",65));
-        queue.enqueue(new Student("Samar",1));
-        queue.enqueue(new Student("Vibhu",10));
-        queue.enqueue(new Student("Shantanu",76));
+        queue.enqueue(new Student("Vimal",58));
+        queue.enqueue(new Student("Varyn",23));
+        queue.enqueue(new Student("Azhar",65));
+        queue.enqueue(new Student("Samay",1));
+        queue.enqueue(new Student("Wasim",10));
+        queue.enqueue(new Student("Sku",76));
 
         queue.show();
         queue.dequeue();
