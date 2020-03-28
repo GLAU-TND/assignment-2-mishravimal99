4b1e9136563cbe3ad7863e8acb9228a62c28053a
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-03-28 12:17:56 +0530

    insert in BST completed

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -7,4 +7,29 @@
 package problem1.mybst;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private TreeNode root;
+    private int countRight;
+    public void insert(int data){
+        TreeNode node=new TreeNode(data);
+        if(root==null){
+            root=node;
+        }else{
+            TreeNode current=root;
+            TreeNode parent=null;
+            while(current!=null){
+                parent=current;
+                if(node.getData()<current.getData()){
+                    current=curent.getLeft();
+                }else{
+                    current=current.getRight();
+                }
+            }
+            if(node.getData()<parent.getData()){
+                parent.setLeft(node);
+            }else{
+                parent.setRight(node);
+            }
+        }
+    }
+
 }
