5fc76847a0a31a497f04db08de6720f73a35986d
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-03-28 12:56:31 +0530

    Main method added

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student("Tushar",58));
+        queue.enqueue(new Student("Tanishq",23));
+        queue.enqueue(new Student("Pavit",65));
+        queue.enqueue(new Student("Samar",1));
+        queue.enqueue(new Student("Vibhu",10));
+        queue.enqueue(new Student("Shantanu",76));
+
+        queue.show();
+        queue.dequeue();
+        queue.show();
+
+    }
 }
