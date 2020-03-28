bc4b9b2f2ec782f181c412c8d88249c0662d523f
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-03-28 12:48:01 +0530

    main method added

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,28 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem2.main.binarySearchTree.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue preQueue = new MyQueue();
+        MyQueue postQueue= new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(preQueue,postQueue);
+        myBinarySearchTree.insert(15);
+        myBinarySearchTree.insert(25);
+        myBinarySearchTree.insert(35);
+        myBinarySearchTree.insert(45);
+        myBinarySearchTree.insert(55);
+        myBinarySearchTree.postOrder(myBinarySearchTree.getRoot());
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder:");
+        preQueue.show();
+        System.out.println("PostOrder:");
+        postQueue.show();
+        System.out.println();
+    }
 }
