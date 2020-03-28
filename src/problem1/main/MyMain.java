971b2ae0355543098d850dedf481b6a3a0a4b512
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-03-28 12:24:18 +0530

    main method added

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,6 +6,21 @@
  */
 package problem1.main;
 // executable class
+import problem1.mybst.MyBinarySearchTree;
 
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
+        binarySearchTree.insert(500);
+        binarySearchTree.insert(400);
+        binarySearchTree.insert(300);
+        binarySearchTree.insert(200);
+        binarySearchTree.insert(100);
+
+        System.out.println("Left childs of the binary search tree: ");
+        binarySearchTree.traverse();
+        System.out.println("No.of Parents not having  left childs are:");
+        System.out.println(binarySearchTree.getCountRight());
+    }
 }
