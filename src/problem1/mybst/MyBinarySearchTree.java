396472fde1e44e404dd9c3282613262b0705d216
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-03-28 12:21:20 +0530

    Traversal method added

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,7 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     private TreeNode root;
@@ -28,6 +29,63 @@ public class MyBinarySearchTree {
                 parent.setLeft(node);
             }else{
                 parent.setRight(node);
+            }
+        }
+    }
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public void preOrder(TreeNode temp) {
+        if (temp == null) {
+            return;
+        } else {
+            System.out.println(temp.getData());
+            preOrder(temp.getLeft());
+            preOrder(temp.getRight());
+        }
+    }
+
+    public void inOrder(TreeNode temp) {
+        if (temp == null) {
+            return;
+        } else {
+
+            inOrder(temp.getLeft());
+            System.out.println(temp.getData());
+            inOrder(temp.getRight());
+        }
+    }
+
+    public int getCountRight() {
+        return countRight;
+    }
+
+    public void setCountRight(int countRight) {
+        this.countRight = countRight;
+    }
+
+    public void traverse() {
+        TreeNode current = root;
+        TreeNode parent = null;
+        this.countRight = 0;
+        while (current != null) {
+            if (current.getLeft() == null) {
+                countRight++;
+            }
+            if (parent.getData() == current.getData()) {
+
+            }
+            if (parent.getData() < current.getData()) {
+                System.out.println(current.getData());
+                current = current.getLeft();
+            }
+            if (parent.getData() > current.getData()) {
+                current = current.getRight();
             }
         }
     }
